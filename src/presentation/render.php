9876461005f25dd20ea85bd15e1354beeb6420f7
Damon Cook <damonacook@gmail.com>
2024-05-31 10:32:54 -0400

    👌 IMPROVE: Presentation block

--- src/presentation/render.php
+++ src/presentation/render.php
@@ -25,7 +25,7 @@
 		data-wp-on--touchstart="actions.onTouchStart"
 		data-wp-on--touchend="actions.onTouchEnd"
 	>
-		<?php echo wp_kses( $content, bounds_kses_extended_ruleset() ); ?>
+		<?php echo $content; ?>
 	</div>
 
 	<div class="presentation-navigation" data-wp-bind--hidden="state.isPresenting">
