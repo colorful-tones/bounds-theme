a9e6666ab1e3c74c26da4cc43c30f5912d6f6b25
Damon Cook <damonacook@gmail.com>
2024-05-31 10:31:09 -0400

    ➕ ADD: four new patterns

---
+++ patterns/giant-footer-text.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Giant Footer Text
+ * Slug: bounds/giant-footer-text
+ */
+?>
+
+<!-- wp:group {"className":"marquee","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group marquee"><!-- wp:group {"className":"marquee-content","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group marquee-content"><!-- wp:site-title {"isLink":false,"style":{"layout":{"selfStretch":"fill","flexSize":null}}} /-->
+
+<!-- wp:site-title {"isLink":false,"style":{"layout":{"selfStretch":"fill","flexSize":null}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: header
+ * Slug: bounds/header
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"var:preset|spacing|20"}},"border":{"radius":"9999px","width":"1px"},"color":{"background":"#ffffffdb"}},"borderColor":"base-2","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide has-border-color has-base-2-border-color has-background" style="border-width:1px;border-radius:9999px;background-color:#ffffffdb;margin-top:var(--wp--preset--spacing--20);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":74,"shouldSyncIcon":true,"className":"is-style-rounded","style":{"color":[]}} /-->
+
+<!-- wp:site-title /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:navigation /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo __('Download', 'bounds');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
